29e51b9ad1d6337092a1ebeeef8bbeddd00b9624
Mohd Sami <94985765+MohdSami234@users.noreply.github.com>
2021-12-30 02:37:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//Assignment -2 Q-2
+//Check whether the given chracter is a vowel or not. (Also check for capital characters).
+
+
+var char = "d";
+
+if(char == "a" || char =="A") {
+ console.log("It is a vowel");
+} else if(char == "E" || char == "E"){
+ console.log("It is a vowel");
+} else if(char == "i" || char =="I") {
+ console.log("It is a vowel");
+} else if(char == "o" || char =="O") {
+ console.log("It is a vowel");
+} else if(char == "u" || char =="U") {
+ console.log("It is a vowel");
+} else {
+ console.log("It is a consonant");
+}
